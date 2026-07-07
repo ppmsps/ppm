e10eacb3a714aac2f6f1ca9fb48ce106dfd458c3
phaphimon sornprasit <hftyd710@gmail.com>
2026-07-07 14:42:18 +0700

    Add files via upload

---
+++ P/dayone.py
@@ -0,0 +1,21 @@
+print("""Hello  World!
+      ยินดีต้อนรับสู่การเขียนโค้ดวันแรก
+      ฉันจะเก่งขึ้นในทุกๆ วัน!""")
+
+numberone = 25
+numbertwo = 7
+result = numberone + numbertwo
+result2 = numberone - numbertwo
+result3 = numberone * numbertwo
+result4 = numberone / numbertwo
+
+print("ผลบวกของ", numberone, "และ", numbertwo, "คือ", result)
+print("ผลลบของ", numberone, "และ", numbertwo, "คือ", result2)
+print("ผลคูณของ", numberone, "และ", numbertwo, "คือ   ", result3)
+print("ผลหารของ", numberone, "และ", numbertwo, "คือ", result4)
+
+
+print("สวัสดีค่ะ เราชื่อ อุ้ม อายุ 19 ปี ตอนนี้กำลังฝึกเขียนโปรแกรมภาษา Python อยู่ค่ะ")
+
+name = input("คุณชื่ออะไรคะ? : ")
+print( "ยินดีต้อนรับคุณ" , name , "เข้าสู้ระบบค่ะ")
